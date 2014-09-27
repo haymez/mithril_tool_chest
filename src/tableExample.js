2ea63590bdb272185601e9651353f4b150ca4907
James Miltenberger <jamilt13@gmail.com>
2014-09-27 17:11:34 +0000

    * Added table sorting functionality * pulled out styles for table classes and column sorting styles

--- src/tableExample.js
+++ src/tableExample.js
@@ -1,13 +1,18 @@
 var tableExample = {
   controller: function() {
-    this.header = ['First Name', 'Last Name', 'Phone Number', 'City', 'State', 'Zip']
-    this.data = [
-      ['James', 'Miltenberger', '9855021337', 'Shreveport', 'Louisiana', '71104'],
-      ['Abbie', 'Miltenberger', '3188347081', 'Shreveport', 'Louisiana', '71104'],
-      ['Walter', 'Miltenberger', '9855021209', 'Houston', 'Texas', '38543'],
-      ['Lauren', 'Miltenberger', '5046743383', 'Houston', 'Texas', '38543'],
+    this.header = ['First Name', 'Last Name', 'Phone Number', 'City', 'State', 'Zip', 'Testing']
+    this.body = [
+      ['James', 'Miltenberger', '9855021337', 'Shreveport', 'Louisiana', '71104', 84],
+      ['Abbie', 'Miltenberger', '3188347081', 'Shreveport', 'Louisiana', '71104', 6],
+      ['Walter', 'Miltenberger', '9855021209', 'Houston', 'Texas', '38543', 19],
+      ['Lauren', 'Miltenberger', '5046743383', 'Houston', 'Texas', '38543', 9],
     ];
-    this.table = new table.controller(this.data, this.header);
+    var styles = {
+      up:    '.fa.fa-arrow-circle-up',
+      down:  '.fa.fa-arrow-circle-down',
+      table: '.pure-table.pure-table-bordered'
+    }
+    this.table = new table.controller(this.header, this.body, styles);
   },
   
   view: function(ctrl) {
--- src/widgets/table.js
+++ src/widgets/table.js
@@ -1,14 +1,48 @@
 var table = {
-  controller: function(rows, header) {
-    this.header = header; //Grab header row
-    this.body = rows;
+
+  controller: function(header, body, opts) {
+    this.header = header;
+    this.body   = body;
+    this.styles = opts;
+    this.currColumn = m.prop();
+    this.reverse = m.prop(false);
+
+    this.sortFunction = function(a, b) {
+      var curr = this.header.indexOf(this.currColumn());
+      if(a[curr] === b[curr]) return 0;
+      else {
+        if(this.reverse())
+          return (a[curr] < b[curr]) ? 1 : -1;
+        else
+          return (a[curr] < b[curr]) ? -1 : 1;
+      }
+    }.bind(this);
+
+    this.sortByColumn = function(value) {
+      if(value === this.currColumn()) this.reverse(!this.reverse());
+      else this.reverse(false);
+      this.currColumn(value);
+    }.bind(this);
+
+    this.getArrow = function(th) {
+      if(th === this.currColumn()) {
+        var direction = (this.reverse()) ? this.styles.down : this.styles.up;
+        return m('i' + direction, {style: 'float:right;'});
+      }
+      else return [];
+    }.bind(this);
+
   },
 
   view: function(ctrl) {
-    return m('table.pure-table.pure-table-bordered', [
+    return m('table' + ctrl.styles.table, [
       m('thead', [
-        m('tr', ctrl.header.map(function(item) {
-          return m('th', item);
+        m('tr', {onclick: ctrl.body.sort(ctrl.sortFunction)}, ctrl.header.map(function(item, index) {
+          var arrow = ctrl.getArrow(item);
+          return m('th', {onclick: m.withAttr('textContent', ctrl.sortByColumn)}, [
+            item,
+            arrow,
+          ]);
         }))
       ]),
       ctrl.body.map(function(row, index) {
@@ -19,28 +53,3 @@ var table = {
     ])
   }
 }
-
-
-// c.multiselect = {
-//   controller: function(tags) {
-
-//     this.tags = tags;
-//     this.tags.splice(0, 1, ''); // Add empty option at beginning of array
-//   },
-
-//   view: function(ctrl, opts) {
-//     var multiselect = m('div.multiselect', [
-//       m('select', { value: ctrl.currentSelection, onclick: m.withAttr('value', opts.add) },
-//         ctrl.tags.map(function(tag) {
-//           return m('option', {disabled: opts.value.indexOf(tag) > -1}, tag);
-//       })),
-//       m('ul', opts.value.map(function(tag) {
-//         return m('li', [
-//           m('span', tag),
-//           m('i.fa.fa-times.fa-fw', {value: tag, onclick: m.withAttr('value', opts.remove)}),
-//         ]);
-//       })),
-//     ]);
-//     return multiselect;
-//   }
-// };
